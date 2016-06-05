42c509395dcff35480385deeda25f396707b823c
TwoEightNine <twoeightnine@list.ru>
2016-06-05 11:39:08 +0400

    16-6-5

---
+++ app/src/main/java/com/ics/twoeightnine/a7400_ics/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.ics.twoeightnine.a7400_ics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
