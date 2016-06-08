1fdd1060b045ddc9bfb86d415a310e13f6729ce7
TwoEightNine <twoeightnine@list.ru>
2016-06-08 11:12:16 +0400

    16-6-8

---
+++ app/src/main/java/com/ics/twoeightnine/a7400_ics/CustomAdapter.java
@@ -0,0 +1,41 @@
+package com.ics.twoeightnine.a7400_ics;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends BaseAdapter {
+    private ArrayList<Item> data;
+    private LayoutInflater inf;
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if(convertView == null)
+            view = inf.inflate(R.layout.row, null);
+        Item item = data.get(position);
+        ((TextView) view.findViewById(R.id.name)).setText(item.getItemName());
+        ((TextView) view.findViewById(R.id.desc)).setText(item.getDescription());
+        return view;
+    }
+}
